ee8f629d2b96436002f884f7fb5eff77bb8acfdd
Fabiano van Gool <fabiano.van.gool@humanoids.nl>
2022-03-17 12:07:58 +0100

    restore test files and rest api services to prevent tests and build from breaking

--- src/components/time-entries/TimeEntries.test.tsx
+++ src/components/time-entries/TimeEntries.test.tsx
@@ -1,32 +1,18 @@
-// import { waitFor } from "@testing-library/react";
-// import { gql } from "@apollo/client";
-// import {Client} from "../../../apollo-client";
-// import { NotFoundError } from "../errors/NotFoundError";
+import { waitFor } from "@testing-library/react";
+import { getTimeEntries } from "../../services/time-entries-api";
+import { NotFoundError } from "../errors/NotFoundError";
 
-// test("if a notFoundError instance is returned after getting a 404", async () => {
-//   const mockFetchResponse = Promise.resolve({
-//     status: 404,
-//   });
-//   global.fetch = jest.fn().mockImplementationOnce(() => mockFetchResponse);
+test("if a notFoundError instance is returned after getting a 404", async () => {
+  const mockFetchResponse = Promise.resolve({
+    status: 404,
+  });
+  global.fetch = jest.fn().mockImplementationOnce(() => mockFetchResponse);
 
-//   const { data } = await Client.query({
-//     query: gql`
-//       query GetTimeEntries {
-//         allTimeEntries {
-//           id
-//           activity
-//           client
-//           stopTimestamp
-//           startTimestamp
-//         };
-// 			}`,
-//   });
+  const response = await getTimeEntries();
 
-//   // const response = data.allTimeEntries;
-
-//   // expect(response).toBeInstanceOf(NotFoundError);
-//   expect(global.fetch).toHaveBeenCalledTimes(1);
-//   waitFor(() =>
-//     expect(fetch).toHaveBeenCalledWith(`${process.env.NEXT_PUBLIC_DB_HOST}/time-entries`),
-//   );
-// });
+  expect(response).toBeInstanceOf(NotFoundError);
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  waitFor(() =>
+    expect(fetch).toHaveBeenCalledWith(`${process.env.NEXT_PUBLIC_DB_HOST}/time-entries`),
+  );
+});
---
+++ src/services/time-entries-api.tsx
@@ -0,0 +1,57 @@
+import { NotFoundError } from "../components/errors/NotFoundError";
+import * as Types from "../components/time-entries/TimeEntries.types";
+
+export async function getTimeEntries(): Promise<Types.TimeEntryProps[]> {
+  return fetch(`${process.env.NEXT_PUBLIC_DB_HOST}/time-entries`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status === 404) {
+        throw new NotFoundError("Did not find time entry");
+      }
+      return response;
+    })
+    .then((response) => response.json())
+    .catch((error) => error);
+}
+
+export async function addTimeEntry(newTimeEntry: Types.TimeEntryProps) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_DB_HOST}/time-entries`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTimeEntry),
+  });
+  return response.json();
+}
+
+export async function removeTimeEntry(id: number) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_DB_HOST}/time-entries/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return response.json();
+}
+
+export async function getClients() {
+  return fetch(`${process.env.NEXT_PUBLIC_DB_HOST}/clients`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status === 404) {
+        throw new NotFoundError("Did not find time entry");
+      }
+      return response;
+    })
+    .then((response) => response.json())
+    .catch((error) => error);
+}
